4c9822b62ce4e2b1809e33f9ef984f8fcfb3f273
Avit Sidis <avit.sidis@gmail.com>
2020-12-21 12:56:48 +0100

    Add project files.

---
+++ src/HouseStock.Api/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HouseStock.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return Ok("Ok");
+        }
+    }
+}
