70917033815653676d4fc08fd421f234c91d329f
Avit Sidis <avit.sidis@gmail.com>
2020-12-29 00:22:05 +0100

    replace api project with blazor app

--- src/HouseStock.Presentation.Blazor/Server/Startup.cs
+++ src/HouseStock.Presentation.Blazor/Server/Startup.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Linq;
 
-namespace HouseStock.Api
+namespace HouseStock.Presentation.Blazor.Server
 {
     public class Startup
     {
@@ -16,9 +18,12 @@ namespace HouseStock.Api
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+
+            services.AddControllersWithViews();
+            services.AddRazorPages();
             DataAccess.Startup.ConfigureServices(services, Configuration.GetConnectionString("stock"));
         }
 
@@ -28,15 +33,23 @@ namespace HouseStock.Api
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseWebAssemblyDebugging();
             }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseBlazorFrameworkFiles();
+            app.UseStaticFiles();
 
             app.UseRouting();
 
-            app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapRazorPages();
                 endpoints.MapControllers();
+                endpoints.MapFallbackToFile("index.html");
             });
         }
     }
