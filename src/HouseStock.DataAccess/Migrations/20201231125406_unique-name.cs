2fbf5b337c9f0ace359df2e060980f784e1cbc54
Avit Sidis <avit.sidis@gmail.com>
2020-12-31 13:55:54 +0100

    ensure names are unique implements #4

---
+++ src/HouseStock.DataAccess/Migrations/20201231125406_unique-name.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HouseStock.DataAccess.Migrations
+{
+    public partial class uniquename : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Shelves_Name",
+                table: "Shelves",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rooms_Name",
+                table: "Rooms",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_Name",
+                table: "Products",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_Name",
+                table: "Categories",
+                column: "Name",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Shelves_Name",
+                table: "Shelves");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Rooms_Name",
+                table: "Rooms");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_Name",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Categories_Name",
+                table: "Categories");
+        }
+    }
+}
