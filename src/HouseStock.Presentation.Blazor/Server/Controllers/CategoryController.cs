6666a7bdd73b715402dd7c32ddb009235521f6a5
Avit Sidis <avit.sidis@gmail.com>
2021-01-01 13:27:58 +0100

    get all categories

---
+++ src/HouseStock.Presentation.Blazor/Server/Controllers/CategoryController.cs
@@ -0,0 +1,36 @@
+﻿using HouseStock.DataAccess;
+using HouseStock.Presentation.Blazor.Shared;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace HouseStock.Presentation.Blazor.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly HouseStockDbContext houseStockDbContext;
+
+        public CategoryController(HouseStockDbContext houseStockDbContext)
+        {
+            this.houseStockDbContext = houseStockDbContext;
+        }
+
+        [HttpGet]
+        public ActionResult<GetAllCategoriesResponse> GetAll()
+        {
+            var all = houseStockDbContext.Categories;
+            return Ok(new GetAllCategoriesResponse
+            {
+                Categories = houseStockDbContext.Categories.Select(category => new GetAllCategoriesResponseItem
+                {
+                    Id = category.Id,
+                    Name = category.Name
+                }).ToList()
+            });
+        }
+
+
+
+    }
+}
