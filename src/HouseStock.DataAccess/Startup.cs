59429660ac5ec48ec11c67950ac484e11bd665c2
Avit Sidis <avit.sidis@gmail.com>
2021-01-01 14:56:31 +0100

    Do not output connection string

--- src/HouseStock.DataAccess/Startup.cs
+++ src/HouseStock.DataAccess/Startup.cs
@@ -7,7 +7,6 @@ namespace HouseStock.DataAccess
     {
         public static void ConfigureServices(IServiceCollection services, string connectionString)
         {
-            System.Console.WriteLine($"CS:{connectionString}");
             services.AddDbContext<HouseStockDbContext>(
                 options => options.UseMySQL(connectionString)
                 );
