3e5876263763435b674b30cdbc7bf055dc02b5cc
Avit Sidis <avit.sidis@gmail.com>
2021-01-01 14:55:46 +0100

    implements #2 add product feature

--- src/HouseStock.Presentation.Blazor/Client/Program.cs
+++ src/HouseStock.Presentation.Blazor/Client/Program.cs
@@ -17,6 +17,7 @@ namespace HouseStock.Presentation.Blazor.Client
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddTransient<RoomControllerProxy>();
             builder.Services.AddTransient<ShelfControllerProxy>();
+            builder.Services.AddTransient<ProductControllerProxy>();
 
             await builder.Build().RunAsync();
         }
---
+++ src/HouseStock.Presentation.Blazor/Client/Services/ProductControllerProxy.cs
@@ -0,0 +1,43 @@
+﻿using HouseStock.Presentation.Blazor.Shared;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace HouseStock.Presentation.Blazor.Client.Services
+{
+    public class ProductControllerProxy
+    {
+        private readonly HttpClient client;
+
+        public ProductControllerProxy(HttpClient client)
+        {
+            this.client = client;
+        }
+
+        public async Task<GetAllCategoriesResponse> GetAllCategories()
+        {
+            var response = await client.GetFromJsonAsync<GetAllCategoriesResponse>("category");
+            return response;
+        }
+
+        public async Task<Response<AddProductResponse>> Add(AddProductRequest roomRequest)
+        {
+            try
+            {
+                var response = await client.PostAsJsonAsync("product", roomRequest);
+                response.EnsureSuccessStatusCode();
+                using var responseStream = await response.Content.ReadAsStreamAsync();
+                var result = await JsonSerializer.DeserializeAsync<AddProductResponse>(responseStream);
+                return Response<AddProductResponse>.Success(result);
+            }
+            catch (System.Exception e)
+            {
+                return Response<AddProductResponse>.Fail("ADD_PRODUCT_ERROR", e);
+            }
+
+        }
+    }
+}
