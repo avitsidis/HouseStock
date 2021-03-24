14748c5f8b76bb0c8e2299d1f6d3d339e3210f06
Avit Sidis <avit.sidis@gmail.com>
2021-03-25 00:53:16 +0100

    feat:13 add version

---
+++ src/HouseStock.Presentation.Blazor/Client/Services/VersionControllerProxy.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HouseStock.Presentation.Blazor.Client.Services
+{
+    public class VersionControllerProxy
+    {
+        private HttpClient client;
+        private ILogger<ProductControllerProxy> logger;
+
+        private string serverVersion = null;
+
+        public VersionControllerProxy(HttpClient client, ILogger<ProductControllerProxy> logger)
+        {
+            this.client = client;
+            this.logger = logger;
+        }
+
+        public async Task<string> GetServerVersion()
+        {
+            if (serverVersion == null)
+            {
+                var response = await client.GetAsync("version");
+                response.EnsureSuccessStatusCode();
+                serverVersion = await response.Content.ReadAsStringAsync();
+                logger.LogInformation("Loaded Server version {version}", serverVersion);
+            }
+            return serverVersion;
+        }
+    }
+}
---
+++ src/HouseStock.Presentation.Blazor/Server/Controllers/VersionController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HouseStock.Presentation.Blazor.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class VersionController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return Ok(this.GetType().Assembly.GetName().Version.ToString());
+        }
+    }
+}
